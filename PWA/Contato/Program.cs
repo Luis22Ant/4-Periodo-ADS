4d08420c06cc051cb82f0e83575c8ca0cc3c62e9
Luis Antonio <luisengenhariaeletronica@gmail.com>
2024-03-02 11:46:39 -0300

    Aula pwa

---
+++ PWA/Contato/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Contato.models;
+using Microsoft.Data.Sqlite;
+using Dapper;
+
+
+Contatos obj = new Contatos
+{
+    Id = "9",
+    Nome = "Pedro",
+    Email = "pedro@gmail.com"
+};
+
+string connectionString = @"Data Source=db/dados.db";
+string query = "INSERT INTO Contatos (Id, Nome, Email) VALUES (@Id, @Nome, @Email)";
+
+try
+{
+    using (var conexao = new SqliteConnection(connectionString))
+    {
+        conexao.Open();
+        conexao.Execute(query, obj);
+
+        Console.WriteLine("Inserido com sucesso!");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Erro : " + ex.ToString());
+}
+
