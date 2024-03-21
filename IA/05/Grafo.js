a869ab813a40a90ada0aa28e151a54caa0ae2c22
Luis Antonio <luisengenhariaeletronica@gmail.com>
2024-03-21 15:54:34 -0300

    Tetse

--- IA/05/Grafo.js
+++ IA/05/Grafo.js
@@ -1,3 +1,5 @@
+import Rastreador from "./Rastreador";
+
 export default class Grafo {
     inicio = null;
     meta = null;
@@ -5,5 +7,28 @@ export default class Grafo {
     constructor(inicio, meta) {
         this.inicio = inicio;
         this.meta = meta;
+    }
+
+    buscaMelhorEscolha(){
+        let fila = [];
+        let ordenados = [];
+        let atual;
+
+        fila.push(new Rastreador(this.inicio,null));
+
+        while(fila.length > 0){
+            atual = fila.shift();
+            if(atual.vertice == this.meta){
+                return atual;
+            }else{
+                ordenados = atual.vertice.getOrdenados();
+                ordenados.forEach(adjacente => {
+                    fila.push(new Rastreador(adjacente,atual));
+                });
+            }
+        }
+
+        return null;
+
     }
 }
